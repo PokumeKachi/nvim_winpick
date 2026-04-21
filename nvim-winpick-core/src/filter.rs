04b81bf81544662894344bcfad9fcedc396d6a38
MarcusGrass <marcus.grass@protonmail.com>
2026-04-21 19:47:41 +0200

    fix: neovim 0.12 compatibility Breaks with neovim 0.11 because of changes to `echo`. Theoretically this wouldn't have to break (with a crash), but I don't feel like figuring out the nvim version at runtime

--- nvim-winpick-core/src/filter.rs
+++ nvim-winpick-core/src/filter.rs
@@ -1,7 +1,8 @@
-use anyhow::Context;
-use nvim_oxi::api::Window;
-
 use crate::opts::FilterRules;
+use anyhow::Context;
+use nvim_oxi::api::opts::OptionOpts;
+use nvim_oxi::api::{get_option_value, Window};
+use std::path::Path;
 
 impl FilterRules {
     pub(crate) fn filter(&self, target_win: &Window, current_win: &Window) -> anyhow::Result<bool> {
@@ -23,24 +24,21 @@ impl FilterRules {
             || !self.bo.buftype.is_empty()
         {
             let buf = target_win.get_buf().context("failed to get window buf")?;
-            // Alternative is umapped in nvim_oxi so far
-            #[allow(deprecated)]
+
             if !self.bo.filetype.is_empty() {
-                let ft: Option<String> = buf
-                    .get_option("filetype")
-                    .context("failed to get buf filetype")?;
+                let ft: Option<String> =
+                    get_option_value("filetype", &OptionOpts::builder().buf(buf.clone()).build())
+                        .context("failed to get buf filetype")?;
                 if let Some(ft) = ft {
                     if self.bo.filetype.iter().any(|filter_ft| filter_ft == &ft) {
                         return Ok(false);
                     }
                 }
             }
-            // Alternative is unmapped in nvim_oxi so far
-            #[allow(deprecated)]
             if !self.bo.buftype.is_empty() {
-                let bt: Option<String> = buf
-                    .get_option("buftype")
-                    .context("failed to get buf buftype")?;
+                let bt: Option<String> =
+                    get_option_value("buftype", &OptionOpts::builder().buf(buf.clone()).build())
+                        .context("failed to get buf buftype")?;
                 if let Some(ft) = bt {
                     if self.bo.buftype.iter().any(|filter_ft| filter_ft == &ft) {
                         return Ok(false);
@@ -49,7 +47,7 @@ impl FilterRules {
             }
             if !self.file_name_contains.is_empty() || !self.file_path_contains.is_empty() {
                 let buf_file = buf.get_name().context("failed to get buf file")?;
-                let path_utf8 = buf_file.display().to_string();
+                let path_utf8 = buf_file.to_string();
                 if self
                     .file_path_contains
                     .iter()
@@ -58,7 +56,7 @@ impl FilterRules {
                     return Ok(false);
                 }
                 if !self.file_name_contains.is_empty() {
-                    if let Some(file_name) = buf_file.file_name() {
+                    if let Some(file_name) = Path::new(&path_utf8).file_name() {
                         if let Some(utf8) = file_name.to_str() {
                             if self.file_name_contains.iter().any(|f| utf8.contains(f)) {
                                 return Ok(false);
--- nvim-winpick-core/src/notify.rs
+++ nvim-winpick-core/src/notify.rs
@@ -1,17 +1,13 @@
-use nvim_oxi::Dictionary;
+use nvim_oxi::api::opts::EchoOpts;
 
 pub(crate) fn notify_error(msg: &str) {
-    let _ = nvim_oxi::api::notify(
-        msg,
-        nvim_oxi::api::types::LogLevel::Error,
-        &Dictionary::default(),
+    let _ = nvim_oxi::api::echo(
+        [(msg, None::<&str>)],
+        true,
+        &EchoOpts::builder().err(true).build(),
     );
 }
 
 pub(crate) fn notify_warn(msg: &str) {
-    let _ = nvim_oxi::api::notify(
-        msg,
-        nvim_oxi::api::types::LogLevel::Warn,
-        &Dictionary::default(),
-    );
+    let _ = nvim_oxi::api::echo([(msg, None::<&str>)], true, &EchoOpts::default());
 }
